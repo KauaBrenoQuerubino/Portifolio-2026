de0f82aa83e5e0ccfad7c71ef20cc8658904d07e
Kaua Breno <kauabrenojobs@gmail.com>
2026-03-03 15:26:35 -0300

    Area de sobre desenvolvida

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,19 @@ import { RouterOutlet } from '@angular/router';
 import { BannerComponent } from "./components/banner/banner.component";
 import { NavComponent } from "./components/nav/nav.component";
 import { SobreComponent } from "./components/sobre/sobre.component";
+import { SkillsComponent } from './components/skills/skills.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, BannerComponent, NavComponent, SobreComponent],
+  imports: [ 
+    BannerComponent, 
+    NavComponent, 
+    SobreComponent, 
+    SkillsComponent, 
+    ProjectsComponent
+  ],
+
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
